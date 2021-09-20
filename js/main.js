d195d111e6177b9840a31330d651fa45f92cdffe
Jesus Hilarion Roig Peix <jesus@prosperia.ai>
2021-09-20 16:48:06 -0400

    refractor: Clean main file

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 import { monkeyPatchMediaDevices } from './media-devices.js';
 monkeyPatchMediaDevices();
-console.log('voy a definir el listener')
-
      
+//Listener for detect devices changes
 navigator.mediaDevices.addEventListener('devicechange', async function (event) {
     console.log('device plugged or unplugged, update de info,', event)
     const res = await navigator.mediaDevices.enumerateDevices();
-    console.log(res);
     chrome.runtime.sendMessage('emmdmmooijoidllkobncpgcmedhgfbma', {devicesList: res}, function(response) {
         console.log('deviceVideoId: ',response.farewell);
       });
-    //TODO update de default udio, video, and device list
 });
-var algo="ok"
 
-console.log('ya defini el listener', algo)
+console.log('Listener settled')
