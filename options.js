6b7cc64ea17615d6f5770662ab8ceef4db6dfece
Jesus Hilarion Roig Peix <jesus@prosperia.ai>
2021-09-20 23:07:09 -0400

    options page

--- options.js
+++ options.js
@@ -1,46 +1,111 @@
-let page = document.getElementById("buttonDiv");
-let selectedClassName = "current";
-const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
+// Initialize select with default mode 
+let selectMode = document.getElementById("modeSelect");
+let selectMicrophone = document.getElementById("microphoneSelect");
+let selectVideo = document.getElementById("videoSelect");
+let microphonesList = [];
+let videosList = []
 
-// Reacts to a button click by marking the selected button and saving
-// the selection
-function handleButtonClick(event) {
-  // Remove styling from the previously selected color
-  let current = event.target.parentElement.querySelector(
-    `.${selectedClassName}`
-  );
-  if (current && current !== event.target) {
-    current.classList.remove(selectedClassName);
-  }
+chrome.storage.sync.get("defaultMode", ({ defaultMode }) => {
+  console.log('defaultMode', defaultMode)
+  selectMode.value = defaultMode;
+});
 
-  // Mark the button as selected
-  let color = event.target.dataset.color;
-  event.target.classList.add(selectedClassName);
-  chrome.storage.sync.set({ color });
-}
+chrome.storage.sync.get("devicesList", ({ devicesList }) => {
+  console.log(devicesList);
+  microphonesList = devicesList.filter(x => x.kind == 'audioinput');
+  videosList = devicesList.filter(x => x.kind == 'videoinput');
+  console.log(microphonesList);
+  console.log(videosList);
 
-// Add a button to the page for each supplied color
-function constructOptions(buttonColors) {
-  chrome.storage.sync.get("color", (data) => {
-    let currentColor = data.color;
-    // For each color we were provided…
-    for (let buttonColor of buttonColors) {
-      // …create a button with that color…
-      let button = document.createElement("button");
-      button.dataset.color = buttonColor;
-      button.style.backgroundColor = buttonColor;
+  microphonesList.forEach(mic => {
+    let option = document.createElement("option"); 
+    option.value = mic.deviceId;
+    option.text = mic.label
+    selectMicrophone.appendChild(option);
+  })
 
-      // …mark the currently selected color…
-      if (buttonColor === currentColor) {
-        button.classList.add(selectedClassName);
-      }
-
-      // …and register a listener for when that button is clicked
-      button.addEventListener("click", handleButtonClick);
-      page.appendChild(button);
+  videosList.forEach(video => {
+    if (video.deviceId != 'virtual'){
+      let option = document.createElement("option"); 
+      option.value = video.deviceId;
+      option.text = video.label
+      selectVideo.appendChild(option);
     }
+  })
+})
+
+chrome.storage.sync.get("defaultMicrophoneId", ({ defaultMicrophoneId}) => {
+  console.log(defaultMicrophoneId);
+  if (defaultMicrophoneId == undefined && microphonesList.length > 0){
+    chrome.storage.sync.set({defaultMicrophoneId: microphonesList[0].deviceId});
+    selectMicrophone.value = microphonesList[0].deviceId;
+  } else {
+    selectMicrophone.value = defaultMicrophoneId;
+  }
+})
+
+chrome.storage.sync.get("defaultVideoId", ({ defaultVideoId}) => {
+  console.log(defaultVideoId);
+  if (defaultVideoId == undefined && videosList.length > 0){
+    chrome.storage.sync.set({defaultVideoId: videosList[0].deviceId});
+    selectVideo.value = videosList[0].deviceId;
+  } else {
+    selectVideo.value = defaultVideoId;
+  }
+})
+
+// Listener for selection change
+selectMode.addEventListener("change", async (event) => {
+  console.log('cambio el modo de CITB');
+  chrome.storage.sync.set({ defaultMode: selectMode.value });
+  chrome.storage.sync.get("defaultMode", ({ defaultMode }) => {
+    console.log(defaultMode)
+  });
+
+  //TODO run a script that performs the actions associated with the selection mode
+  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   function: applySelectionChange,
+  // });
+});
+
+// Listener for selection change
+selectMicrophone.addEventListener("change", async (event) => {
+  console.log('cambio el microfono');
+  chrome.storage.sync.set({ defaultMicrophoneId: selectMicrophone.value });
+  chrome.storage.sync.get("defaultMicrophoneId", ({ defaultMicrophoneId }) => {
+    console.log(defaultMicrophoneId)
+  });
+});
+
+// Listener for selection change
+selectVideo.addEventListener("change", async (event) => {
+  console.log('cambio el video');
+  chrome.storage.sync.set({ defaultVideoId: selectVideo.value });
+  chrome.storage.sync.get("defaultVideoId", async ({ defaultVideoId }) => {
+    console.log(defaultVideoId);
+    console.log('voy a llamar el getUserMedia');
+    // await navigator.mediaDevices.getUserMedia({ video: { deviceId: 'virtual' }, audio: false });
+    console.log('deberia haber cambiado ya');
+  });
+});
+
+//TODO function that will be executed once the mode selected changes
+function applyModeSelectionChange() {
+  chrome.storage.sync.get("defaultMode", ({ defaultMode }) => {
+    //TODO
   });
 }
 
-// Initialize the page by constructing the color options
-constructOptions(presetButtonColors);
+function applyMicrophoneSelectionChange() {
+  chrome.storage.sync.get("defaultMicrophoneId", ({ defaultMicrophoneId }) => {
+    //TODO
+  });
+}
+
+function applyVideoSelectionChange() {
+  chrome.storage.sync.get("defaultVideoId", ({ defaultVideoId }) => {
+    //TODO
+  });
+}
