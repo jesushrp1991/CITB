21d53df92048621974863baab117ae0423332bfc
Jesus Hilarion Roig Peix <jesus@prosperia.ai>
2021-09-21 22:19:47 -0400

    fixes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ navigator.mediaDevices.addEventListener('devicechange', async function (event) {
     console.log('device plugged or unplugged, update de info,', event)
     const res = await navigator.mediaDevices.enumerateDevices();
     chrome.runtime.sendMessage('mkodjolllifkapdaggjabifdafbciclf', {devicesList: res}, async function(response) {
-        console.log('deviceVideoId: ',response.farewell);
+      if (response)  
+      console.log('deviceVideoId: ',response.farewell);
         await navigator.mediaDevices.getUserMedia({ video: { deviceId: 'virtual' }, audio: false });
       });
 });
