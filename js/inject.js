c19acdfdb43ab187e10b655b5a1b6720c2c50767
Jesus Hilarion Roig Peix <jesus@prosperia.ai>
2021-09-23 07:51:17 -0400

    inject the pizzicato script to the page

--- js/inject.js
+++ js/inject.js
@@ -1,7 +1,11 @@
 'use strict';
+const script1 = document.createElement('script');
+script1.setAttribute("type", "module");
+script1.setAttribute("src", chrome.runtime.getURL('js/Pizzicato.min.js'));
+const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+head.insertBefore(script1, head.lastChild);
 
 const script = document.createElement('script');
 script.setAttribute("type", "module");
 script.setAttribute("src", chrome.runtime.getURL('js/main.js'));
-const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
 head.insertBefore(script, head.lastChild);
