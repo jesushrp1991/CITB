fa542f52761025250b8e211e51165b0f3f10e068
Jose <Collado>
2021-10-20 07:28:16 -0400

    Environment fixed

--- js/enviroment.js
+++ js/enviroment.js
@@ -1,7 +1,12 @@
 const enviroment = {
-    EXTENSIONID: "pgloinlccpmhpgbnccfecikdjgdhneof",
-    MYVIDEODDEVICELABEL: "Sirius USB2.0 Camera (0ac8:3340)",
-    MYAUDIODEVICELABEL: 'Varios micrófonos (Realtek High Definition Audio)',
+    //**** */ CONFIGURATION FOR CITB & CHROME ORIGINAL ID *****//
+    EXTENSIONID: "ijbdnbhhklnlmdpldichdlknfaibceaf",
+    MYVIDEODDEVICELABEL: "2K HD Camera",
+    MYAUDIODEVICELABEL: 'CITB',
+    //**** */ COLLADO *****//
+    // EXTENSIONID: "pgloinlccpmhpgbnccfecikdjgdhneof",
+    // MYVIDEODDEVICELABEL: "Sirius USB2.0 Camera (0ac8:3340)",
+    // MYAUDIODEVICELABEL: 'Varios micrófonos (Realtek High Definition Audio)',
     // MYAUDIODEVICELABEL: 'Auriculares con micrófono (Redmi AirDots_R Hands-Free) (Bluetooth)',
     fps : 1000/30,
     predictionsCount: 18
