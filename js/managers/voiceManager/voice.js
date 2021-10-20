ad249560fb71bd405248d90d6d2fc1c0f2baf10a
Jose <Collado>
2021-10-20 07:24:05 -0400

    Changes in voice commands

--- js/managers/voiceManager/voice.js
+++ js/managers/voiceManager/voice.js
@@ -1,26 +1,72 @@
 const speachCommands = () => {
 try{
-    var cameraCommands = {
-        'class in the box camera': () => {
+      var cameraCommands = {
+        '*w the box camera': () => {
         //   console.log("Change camera");
           document.getElementsByClassName("CITBCamButton")[0].click(); 
         }
       };
     var showCommands = {
-        'class in the box show': ()=> {
+        '*w the box show': ()=> {
         //   console.log("Show Mode");
           document.getElementsByClassName("CITBShowButton")[0].click(); 
         }
       };
     var classCommands = {
-        'class in the box class': () => {
+        '*w the box class': () => {
         //   console.log("Class Mode");
           document.getElementsByClassName("CITBClassButton")[0].click(); 
         }
-      };
+    };
+    // var cameraSimilarCommands1 = {
+    //     'class in the box come': () => {
+    //     //   console.log("Change camera");
+    //       document.getElementsByClassName("CITBCamButton")[0].click(); 
+    //     }
+    //   };
+    // var cameraSimilarCommands2 = {
+    //     'class in the box came': () => {
+    //     //   console.log("Change camera");
+    //       document.getElementsByClassName("CITBCamButton")[0].click(); 
+    //     }
+    //   };
+    // var cameraSimilarCommands3 = {
+    //     'class in the box calm': () => {
+    //     //   console.log("Change camera");
+    //       document.getElementsByClassName("CITBCamButton")[0].click(); 
+    //     }
+    //   };
+    // var cameraSimilarCommands4 = {
+    //     'class in the box chem': () => {
+    //     //   console.log("Change camera");
+    //       document.getElementsByClassName("CITBCamButton")[0].click(); 
+    //     }
+    //   };
+    // var cameraSimilarCommands5 = {
+    //     'class in the box camera': () => {
+    //     //   console.log("Change camera");
+    //       document.getElementsByClassName("CITBCamButton")[0].click(); 
+    //     }
+    //   };
+    // var showCommands = {
+    //     'class in the box show': ()=> {
+    //     //   console.log("Show Mode");
+    //       document.getElementsByClassName("CITBShowButton")[0].click(); 
+    //     }
+    //   };
+    // var classCommands = {
+    //     'class in the box class': () => {
+    //     //   console.log("Class Mode");
+    //       document.getElementsByClassName("CITBClassButton")[0].click(); 
+    //     }
+    //   };
     
       // Add our commands to annyang
       annyang.addCommands(cameraCommands);
+      // annyang.addCommands(cameraSimilarCommands1);
+      // annyang.addCommands(cameraSimilarCommands2);
+      // annyang.addCommands(cameraSimilarCommands3);
+      // annyang.addCommands(cameraSimilarCommands4);
       annyang.addCommands(showCommands);
       annyang.addCommands(classCommands);
     
