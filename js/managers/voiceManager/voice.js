5bcf61238d483fccef895217d3742fc7c6edc12e
Jose <Collado>
2021-10-20 05:43:43 -0400

    Changing variables names

--- js/managers/voiceManager/voice.js
+++ js/managers/voiceManager/voice.js
@@ -1,28 +1,28 @@
 const speachCommands = () => {
 try{
-    var commands = {
+    var cameraCommands = {
         'class in the box camera': () => {
-          console.log("Change camera");
+        //   console.log("Change camera");
           document.getElementsByClassName("CITBCamButton")[0].click(); 
         }
       };
-    var commands2 = {
+    var showCommands = {
         'class in the box show': ()=> {
-          console.log("Show Mode");
+        //   console.log("Show Mode");
           document.getElementsByClassName("CITBShowButton")[0].click(); 
         }
       };
-    var commands3 = {
+    var classCommands = {
         'class in the box class': () => {
-          console.log("Class Mode");
+        //   console.log("Class Mode");
           document.getElementsByClassName("CITBClassButton")[0].click(); 
         }
       };
     
       // Add our commands to annyang
-      annyang.addCommands(commands);
-      annyang.addCommands(commands2);
-      annyang.addCommands(commands3);
+      annyang.addCommands(cameraCommands);
+      annyang.addCommands(showCommands);
+      annyang.addCommands(classCommands);
     
       // Start listening. You can call this here, or attach this call to an event, button, etc.
       annyang.start();
