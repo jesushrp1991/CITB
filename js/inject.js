19ee0822814852ca55132aae8fb3cd1da288657e
Julio Hernandez <jahp8719@gmail.com>
2021-10-22 21:40:47 -0400

    feact: last update

--- js/inject.js
+++ js/inject.js
@@ -1,5 +1,4 @@
 'use strict';
-<<<<<<< HEAD
 if (window.location.host === 'meet.google.com' || window.location.host.includes('zoom.us') || window.location.host == 'teams.live.com') {
     
     const help = document.createElement('script');
@@ -8,16 +7,11 @@ if (window.location.host === 'meet.google.com' || window.location.host.includes(
     const helphead = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
     helphead.insertBefore(help, helphead.firstChild);
     
-=======
-if (window.location.host === 'meet.google.com'){
-
->>>>>>> f6df13b762f40f667cf676b26c1e289b07b4e527
     const script = document.createElement('script');
     script.setAttribute("type", "module");
     script.setAttribute("src", chrome.runtime.getURL('js/main.js'));
     const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
     head.insertBefore(script, head.firstChild);
-<<<<<<< HEAD
     
     const annyangScript = document.createElement('script');
     annyangScript.setAttribute("type", "module");
@@ -25,6 +19,4 @@ if (window.location.host === 'meet.google.com'){
     const annyangScriptHead = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
     annyangScriptHead.insertBefore(annyangScript, annyangScriptHead.firstChild);
 
-=======
->>>>>>> f6df13b762f40f667cf676b26c1e289b07b4e527
 }
