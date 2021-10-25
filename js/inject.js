221931014e2e9463392c7e40a460981bafa6e533
Jose <Collado>
2021-10-25 06:48:09 -0400

    Centering content

--- js/inject.js
+++ js/inject.js
@@ -1,12 +1,6 @@
 'use strict';
 if (window.location.host === 'meet.google.com' || window.location.host.includes('zoom.us') || window.location.host == 'teams.live.com' || window.location.host == 'teams.microsoft.com') {
     
-    const script1 = document.createElement('script');
-    script1.setAttribute("type", "module");
-    script1.setAttribute("src", chrome.runtime.getURL('/helper/js/help.js'));
-    const head1 = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
-    head1.insertBefore(script1, head1.firstChild);
-    
     const script = document.createElement('script');
     script.setAttribute("type", "module");
     script.setAttribute("src", chrome.runtime.getURL('js/main.js'));
@@ -21,8 +15,6 @@ if (window.location.host === 'meet.google.com' || window.location.host.includes(
             console.log("iframe",iFrame);
             const documentBody = document.body || document.getElementsByTagName("body")[0] ;
             documentBody.insertBefore (iFrame, documentBody.firstChild);
-        
-           
         }
     }
    
