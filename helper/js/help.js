cd3061ca77a553346241b4db43f0497062f76608
Jose <Collado>
2021-10-25 08:37:46 -0400

    Only one HTML whit inline JS

--- helper/js/help.js
+++ helper/js/help.js
@@ -1,6 +1,22 @@
-function myFunction() {
-    alert("My function")
-  }
-export {
-    myFunction
+window.imgCount = 2;
+const helpFunction = () => {
+    let collado = document.getElementById("collado");
+    console.log("collado",collado);
+    collado.addEventListener("click",()=>{
+        changeImg(window.imgCount);
+    })
 }
+const changeImg = (imgCount) =>{
+    let imgTag = document.getElementById("imgTag");
+    console.log("imgCount",imgCount);
+    if(imgCount >= 8){
+        console.log("Entró");
+        parent.closeIFrame();
+    }else{
+        imgTag.src = chrome.runtime.getURL (`helper/img/${imgCount}.png`);
+    }
+    window.imgCount ++;
+}
+
+
+helpFunction();
--- js/inject.js
+++ js/inject.js
@@ -1,6 +1,6 @@
 'use strict';
 if (window.location.host === 'meet.google.com' || window.location.host.includes('zoom.us') || window.location.host == 'teams.live.com' || window.location.host == 'teams.microsoft.com') {
-    
+        
     const script = document.createElement('script');
     script.setAttribute("type", "module");
     script.setAttribute("src", chrome.runtime.getURL('js/main.js'));
@@ -10,11 +10,15 @@ if (window.location.host === 'meet.google.com' || window.location.host.includes(
     document.onreadystatechange = (event) => {
         if (document.readyState == 'complete'){ 
             var iFrame  = document.createElement ("iframe");
-            iFrame.setAttribute('id','iframe');
+            iFrame.setAttribute('id','iframeHelp');
             iFrame.src  = chrome.runtime.getURL ("/helper/index.html");
-            console.log("iframe",iFrame);
             const documentBody = document.body || document.getElementsByTagName("body")[0] ;
             documentBody.insertBefore (iFrame, documentBody.firstChild);
+
+            // const script1 = document.createElement('script');
+            // script1.setAttribute("type", "module");
+            // script1.setAttribute("src", chrome.runtime.getURL('/helper/js/help.js'));
+            // documentBody.insertBefore (script1, documentBody.lastChild);
         }
     }
    
