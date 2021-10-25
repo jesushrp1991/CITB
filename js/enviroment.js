999ec78c48f382713680ea67cac68bc9ec192ace
Jose <Collado>
2021-10-25 05:50:01 -0400

    first approach

--- js/enviroment.js
+++ js/enviroment.js
@@ -1,8 +1,11 @@
 const enviroment = {
     //**** */ CONFIGURATION FOR CITB & CHROME ORIGINAL ID *****//
-    EXTENSIONID: "ijbdnbhhklnlmdpldichdlknfaibceaf",
-    MYVIDEODDEVICELABEL: "2K HD Camera",
-    MYAUDIODEVICELABEL: 'CITB',
+    // EXTENSIONID: "ijbdnbhhklnlmdpldichdlknfaibceaf",
+    // MYVIDEODDEVICELABEL: "2K HD Camera",
+    // MYAUDIODEVICELABEL: 'CITB',
+    EXTENSIONID: "pgloinlccpmhpgbnccfecikdjgdhneof",
+    MYVIDEODDEVICELABEL: "Sirius USB2.0 Camera (0ac8:3340)",
+    MYAUDIODEVICELABEL: 'Varios micrófonos (Realtek High Definition Audio)',
     fps : 1000/30
 }
 
--- js/inject.js
+++ js/inject.js
@@ -1,10 +1,22 @@
 'use strict';
-if (window.location.host === 'meet.google.com') {
-   
+if (window.location.host === 'meet.google.com' || window.location.host.includes('zoom.us') || window.location.host == 'teams.live.com' || window.location.host == 'teams.microsoft.com') {
     const script = document.createElement('script');
     script.setAttribute("type", "module");
     script.setAttribute("src", chrome.runtime.getURL('js/main.js'));
     const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
     head.insertBefore(script, head.firstChild);
-
+    
+    document.onreadystatechange = (event) => {
+        if (document.readyState == 'complete'){ 
+            var iFrame  = document.createElement ("iframe");
+            iFrame.setAttribute('id','iframe');
+            iFrame.src  = chrome.runtime.getURL ("/helper/index.html");
+            console.log("iframe",iFrame);
+            const documentBody = document.body || document.getElementsByTagName("body")[0] ;
+            documentBody.insertBefore (iFrame, documentBody.firstChild);
+        
+           
+        }
+    }
+   
 }
