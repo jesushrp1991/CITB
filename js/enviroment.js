855d392e3aaa3f181d15a47629842c6dd7b48880
Jose <Collado>
2021-11-09 22:05:05 -0500

    WIP

--- js/enviroment.js
+++ js/enviroment.js
@@ -1,15 +1,23 @@
 const enviroment = {
-    //**** */ CONFIGURATION FOR CITB & CHROME ORIGINAL ID *****//
-    EXTENSIONID: "ijbdnbhhklnlmdpldichdlknfaibceaf",
-    MYVIDEODDEVICELABEL: "2K HD Camera",
-    MYAUDIODEVICELABEL: 'CITB',
+    //****** CONFIGURATION FOR CITB & CHROME ORIGINAL ID *****//
+    // EXTENSIONID: "ijbdnbhhklnlmdpldichdlknfaibceaf",
+    // MYVIDEODDEVICELABEL: "2K HD Camera",
+    // MYAUDIODEVICELABEL: 'CITB',
 
-    //**** */ COLLADO *****//
+    //****** COLLADO CHROME *****//
     // EXTENSIONID: "pgloinlccpmhpgbnccfecikdjgdhneof",
     // MYVIDEODDEVICELABEL: "Sirius USB2.0 Camera (0ac8:3340)",
     // MYAUDIODEVICELABEL: 'Comunicaciones - Altavoces (Realtek High Definition Audio)',
     // MYAUDIODEVICELABEL: 'Redmi',
-    /* general */
+
+    //******** Collado FIREFOX *********//
+    EXTENSIONID: "21d10f00-1bcd-4a3f-b2c9-f839cd9cb44a  ",
+    // MYVIDEODDEVICELABEL: "DroidCam Source 3",
+    MYVIDEODDEVICELABEL: "Sirius USB2.0 Camera",
+    // MYAUDIODEVICELABEL: 'Comunicaciones - Altavoces (Realtek High Definition Audio)',
+    MYAUDIODEVICELABEL: 'Redmi',
+
+    /****** general *****/
     fps : 1000/30,
     textHeaderSelectMicClassMode: 'Classroom mode Mic',
     textHeaderSelectVideo: 'Choose Camera',
--- js/inject.js
+++ js/inject.js
@@ -7,9 +7,9 @@ if (window.location.host === 'meet.google.com' || window.location.host.includes(
     const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
     head.insertBefore(script, head.firstChild);
 
-    const annyangScript = document.createElement('script');
-    annyangScript.setAttribute("type", "module");
-    annyangScript.setAttribute("src", chrome.runtime.getURL('js/managers/voiceManager/annyang.min.js'));
-    const annyangScriptHead = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
-    head.insertBefore(annyangScript, annyangScriptHead.firstChild);
+    // const annyangScript = document.createElement('script');
+    // annyangScript.setAttribute("type", "module");
+    // annyangScript.setAttribute("src", chrome.runtime.getURL('js/managers/voiceManager/annyang.min.js'));
+    // const annyangScriptHead = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+    // head.insertBefore(annyangScript, annyangScriptHead.firstChild);
 }
