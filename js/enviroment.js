d2af3e238779d97d1bc5dca83558c5769f110e4f
Jose <Collado>
2021-11-25 17:14:59 -0500

    Environment

--- js/enviroment.js
+++ js/enviroment.js
@@ -1,14 +1,14 @@
 const enviroment = {
     //**** */ CONFIGURATION FOR CITB & CHROME ORIGINAL ID *****//
     // EXTENSIONID: "ijbdnbhhklnlmdpldichdlknfaibceaf",
-    MYVIDEODDEVICELABEL: "2K HD Camera, PC Camera",
-    MYAUDIODEVICELABEL: 'CITB',
+    // MYVIDEODDEVICELABEL: "2K HD Camera, PC Camera",
+    // MYAUDIODEVICELABEL: 'CITB',
 
     //**** */ COLLADO *****//
     EXTENSIONID: "pgloinlccpmhpgbnccfecikdjgdhneof",
     // MYVIDEODDEVICELABEL: ["2K HD Camera","PC Camera"],
-    // MYVIDEODDEVICELABEL: "Sirius USB2.0 Camera (0ac8:3340)",
-    // MYAUDIODEVICELABEL: 'Comunicaciones - Micrófono (Dispositivo de High Definition Audio)',
+    MYVIDEODDEVICELABEL: "Sirius USB2.0 Camera (0ac8:3340), PC Camera",
+    MYAUDIODEVICELABEL: 'Comunicaciones - Micrófono (Dispositivo de High Definition Audio)',
     // MYAUDIODEVICELABEL: 'Redmi',
     /* general */
     fps : 1000/30,
