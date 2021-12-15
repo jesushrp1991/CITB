42d7f674b602d23190ca3469fe8f775450fe718a
Jose <Collado>
2021-12-15 13:37:07 -0500

    First approach: new extension whit google auth + upload working

--- CITB_Rec/js/external/oauth2/injection.js
+++
@@ -1,3 +0,0 @@
-window.location =
-  chrome.extension.getURL("js/external/oauth2/oauth2.html") +
-  window.location.href.substring(window.location.href.indexOf("?"));
--- CITB_Rec/popup.js
+++ CITB_Rec/popup.js
@@ -1,5 +1,107 @@
-const auth = () =>{
-  window.oauth2.start();
+const API_KEY = 'AIzaSyDhLKHKTBWjlDSrRLPY_-kvgV0xcJH7qd0';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+function onGAPILoad() {
+  gapi.client.init({
+    // Don't pass client nor scope as these will init auth2, which we don't want
+    apiKey: API_KEY,
+    discoveryDocs: DISCOVERY_DOCS,
+  }).then(function () {
+    console.log('gapi initialized')
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      gapi.auth.setToken({
+        'access_token': token,
+      });
+    })
+  }, function(error) {
+    console.log('error', error)
+  });
 }
 
-document.getElementById('authButton').addEventListener('click',auth);
+
+
+async function listFiles  () {
+  retrieveAllFiles(displayItems);        
+}
+
+function displayItems (result) {
+  console.log("response",result)
+  console.log('Files:');
+  if (result && result.length > 0) {
+      for (var i = 0; i<result.length; i++) {
+          var file = result[i];
+          console.log(file.name + ' (' + file.id + ')');
+      }
+  } else {
+  console.log('No files found.');
+  }
+}
+
+function retrieveAllFiles(callback) {
+  var initialRequest = gapi.client.drive.files.list({});
+  initialRequest.execute((resp)=>{
+      console.log("resp",resp.files,resp['files']);
+      displayItems(resp.files);
+  })        
+}
+
+const getAuthToken = () =>{
+  chrome.identity.getAuthToken({interactive: true}, function(token) {
+    console.log('got the token', token);
+    listFiles();
+  })
+};
+
+
+/*
+  *   Upload to Drive
+  *
+*/ 
+document.getElementById("uploadfile").addEventListener("change", run);
+
+  function run(obj) {
+    const file = obj.target.files[0];
+    if (file.name != "") {
+      let fr = new FileReader();
+      fr.fileName = file.name;
+      fr.fileSize = file.size;
+      fr.fileType = file.type;
+      fr.readAsArrayBuffer(file);
+      fr.onload = resumableUpload;
+    }
+  }
+
+  function resumableUpload(e) {
+    accessToken = gapi.auth.getToken().access_token; // Please set access token here.
+    console.log("accessToken",accessToken)
+    document.getElementById("progress").innerHTML = "Initializing.";
+    const f = e.target;
+    const resource = {
+      fileName: f.fileName,
+      fileSize: f.fileSize,
+      fileType: f.fileType,
+      fileBuffer: f.result,
+      accessToken: accessToken,
+    };
+    const ru = new ResumableUploadToGoogleDrive();
+    ru.Do(resource, function (res, err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(res);
+      let msg = "";
+      if (res.status == "Uploading") {
+        msg =
+          Math.round(
+            (res.progressNumber.current / res.progressNumber.end) * 100
+          ) + "%";
+      } else {
+        msg = res.status;
+      }
+      document.getElementById("progress").innerText = msg;
+    });
+  }
+
+const authButton = document.getElementById('authButton');
+authButton.addEventListener('click',getAuthToken);
