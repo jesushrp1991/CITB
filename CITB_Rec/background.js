a0285dba8744061c3d1a095829bd8f76611a48f3
Jose <Collado>
2021-12-16 01:23:04 -0500

    Recording and uploading sucessfully to Drive!!!

--- CITB_Rec/background.js
+++ CITB_Rec/background.js
@@ -1,18 +1,100 @@
-  //TEST DESKTOP CAPTURE
+const API_KEY = 'AIzaSyDhLKHKTBWjlDSrRLPY_-kvgV0xcJH7qd0';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+function onGAPILoad() {
+  gapi.client.init({
+    // Don't pass client nor scope as these will init auth2, which we don't want
+    apiKey: API_KEY,
+    discoveryDocs: DISCOVERY_DOCS,
+  }).then(function () {
+    console.log('gapi initialized')
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      gapi.auth.setToken({
+        'access_token': token,
+      });
+    })
+  }, function(error) {
+    console.log('error', error)
+  });
+}
+
+const getAuthToken = () =>{
+  chrome.identity.getAuthToken({interactive: true}, function(token) {
+    console.log('got the token', token);
+  })
+};
+
+
+/*
+  *   Upload to Drive
+  *
+*/ 
+  function run(obj) {
+    // const file = obj.target.files[0];
+    const file = obj;
+    if (file.name != "") {
+      let fr = new FileReader();
+      fr.fileName = file.name;
+      fr.fileSize = file.size;
+      fr.fileType = file.type;
+      fr.readAsArrayBuffer(file);
+      fr.onload = resumableUpload;
+    }
+  }
+
+  function resumableUpload(e) {
+    accessToken = gapi.auth.getToken().access_token; // Please set access token here.
+    // console.log("accessToken",accessToken)
+    // document.getElementById("progress").innerHTML = "Initializing.";
+    const f = e.target;
+    const resource = {
+      fileName: f.fileName,
+      fileSize: f.fileSize,
+      fileType: f.fileType,
+      fileBuffer: f.result,
+      accessToken: accessToken,
+    };
+    const ru = new ResumableUploadToGoogleDrive();
+    ru.Do(resource, function (res, err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    //   console.log(res);
+    //   let msg = "";
+    //   if (res.status == "Uploading") {
+    //     msg =
+    //       Math.round(
+    //         (res.progressNumber.current / res.progressNumber.end) * 100
+    //       ) + "%";
+    //   } else {
+    //     msg = res.status;
+    //   }
+    //   document.getElementById("progress").innerText = msg;
+    });
+  }
+
+getAuthToken()
+
+  /* 
+  ** DESKTOP REC
+  */
 let chunks = [];
 const download = () => {
     console.log("Hello download")
     var blob = new Blob(chunks, {
         type: "video/webm"
     });
-    var url = URL.createObjectURL(blob);
-    var a = document.createElement("a");
-    document.body.appendChild(a);
-    a.style = "display: none";
-    a.href = url;
-    a.download = "test.webm";
-    a.click();
-    window.URL.revokeObjectURL(url);
+    var file = new File([blob], "CITB REC " + Date() + ".webm");
+    run(file);
+    // var url = URL.createObjectURL(blob);
+    // var a = document.createElement("a");
+    // document.body.appendChild(a);
+    // a.style = "display: none";
+    // a.href = url;
+    // a.download = "test.webm";
+    // a.click();
+    // window.URL.revokeObjectURL(url);
 }
   
 const captureScreen = async()=> {
--- CITB_Rec/popup.js
+++ CITB_Rec/popup.js
@@ -1,76 +1,76 @@
-const API_KEY = 'AIzaSyDhLKHKTBWjlDSrRLPY_-kvgV0xcJH7qd0';
-const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+// const API_KEY = 'AIzaSyDhLKHKTBWjlDSrRLPY_-kvgV0xcJH7qd0';
+// const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 
-function onGAPILoad() {
-  gapi.client.init({
-    // Don't pass client nor scope as these will init auth2, which we don't want
-    apiKey: API_KEY,
-    discoveryDocs: DISCOVERY_DOCS,
-  }).then(function () {
-    console.log('gapi initialized')
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-      gapi.auth.setToken({
-        'access_token': token,
-      });
-    })
-  }, function(error) {
-    console.log('error', error)
-  });
-}
+// function onGAPILoad() {
+//   gapi.client.init({
+//     // Don't pass client nor scope as these will init auth2, which we don't want
+//     apiKey: API_KEY,
+//     discoveryDocs: DISCOVERY_DOCS,
+//   }).then(function () {
+//     console.log('gapi initialized')
+//     chrome.identity.getAuthToken({interactive: true}, function(token) {
+//       gapi.auth.setToken({
+//         'access_token': token,
+//       });
+//     })
+//   }, function(error) {
+//     console.log('error', error)
+//   });
+// }
 
-const getAuthToken = () =>{
-  chrome.identity.getAuthToken({interactive: true}, function(token) {
-    console.log('got the token', token);
-  })
-};
+// const getAuthToken = () =>{
+//   chrome.identity.getAuthToken({interactive: true}, function(token) {
+//     console.log('got the token', token);
+//   })
+// };
 
 
-/*
-  *   Upload to Drive
-  *
-*/ 
-  function run(obj) {
-    const file = obj.target.files[0];
-    if (file.name != "") {
-      let fr = new FileReader();
-      fr.fileName = file.name;
-      fr.fileSize = file.size;
-      fr.fileType = file.type;
-      fr.readAsArrayBuffer(file);
-      fr.onload = resumableUpload;
-    }
-  }
+// /*
+//   *   Upload to Drive
+//   *
+// */ 
+//   function run(obj) {
+//     const file = obj.target.files[0];
+//     if (file.name != "") {
+//       let fr = new FileReader();
+//       fr.fileName = file.name;
+//       fr.fileSize = file.size;
+//       fr.fileType = file.type;
+//       fr.readAsArrayBuffer(file);
+//       fr.onload = resumableUpload;
+//     }
+//   }
 
-  function resumableUpload(e) {
-    accessToken = gapi.auth.getToken().access_token; // Please set access token here.
-    console.log("accessToken",accessToken)
-    document.getElementById("progress").innerHTML = "Initializing.";
-    const f = e.target;
-    const resource = {
-      fileName: f.fileName,
-      fileSize: f.fileSize,
-      fileType: f.fileType,
-      fileBuffer: f.result,
-      accessToken: accessToken,
-    };
-    const ru = new ResumableUploadToGoogleDrive();
-    ru.Do(resource, function (res, err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(res);
-      let msg = "";
-      if (res.status == "Uploading") {
-        msg =
-          Math.round(
-            (res.progressNumber.current / res.progressNumber.end) * 100
-          ) + "%";
-      } else {
-        msg = res.status;
-      }
-      document.getElementById("progress").innerText = msg;
-    });
-  }
+//   function resumableUpload(e) {
+//     accessToken = gapi.auth.getToken().access_token; // Please set access token here.
+//     console.log("accessToken",accessToken)
+//     document.getElementById("progress").innerHTML = "Initializing.";
+//     const f = e.target;
+//     const resource = {
+//       fileName: f.fileName,
+//       fileSize: f.fileSize,
+//       fileType: f.fileType,
+//       fileBuffer: f.result,
+//       accessToken: accessToken,
+//     };
+//     const ru = new ResumableUploadToGoogleDrive();
+//     ru.Do(resource, function (res, err) {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log(res);
+//       let msg = "";
+//       if (res.status == "Uploading") {
+//         msg =
+//           Math.round(
+//             (res.progressNumber.current / res.progressNumber.end) * 100
+//           ) + "%";
+//       } else {
+//         msg = res.status;
+//       }
+//       document.getElementById("progress").innerText = msg;
+//     });
+//   }
 
-getAuthToken()
+// getAuthToken()
