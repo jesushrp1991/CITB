79f8c3882593d1b25d068d507481e04f88575e5b
Jose <Collado>
2021-12-29 02:40:55 -0500

    WIP Layout CITB RECs

--- CITB_Rec/popup.js
+++ CITB_Rec/popup.js
@@ -120,8 +120,21 @@ const populateMicSelect = async () => {
 
 
 const localDownload = () => {
-    const request = { recordingStatus: 'localDownload' };
-    sendMessage(request);
+    chrome.tabs.create({active: false}, function(newTab) {
+        // After the tab has been created, open a window to inject the tab into it.
+        // chrome.windows.create({
+        //     tabId:      newTab.id,
+        //     type:       "panel",
+        //     url:        chrome.extension.getURL('videoManager.html'),
+        //     focused: true
+        // },function(window){
+        //     winID = newWindow.id;
+        // });
+        chrome.tabs.create({ url: chrome.extension.getURL('videoManager.html') });
+
+    });
+    // const request = { recordingStatus: 'localDownload' };
+    // sendMessage(request);
 }
 
 const getShareLink = () =>{
