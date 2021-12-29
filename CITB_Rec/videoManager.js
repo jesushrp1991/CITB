47a7b68913ccb9b24f75594467fdd6e07a454f83
Jose <Collado>
2021-12-29 10:50:32 -0500

    Creating personalized Card

--- CITB_Rec/videoManager.js
+++ CITB_Rec/videoManager.js
@@ -3,16 +3,22 @@ const escapeHTMLPolicy = trustedTypes.createPolicy("forceInner", {
 })
 
 
-const readElement = () => {
+const createRecordCard = () => {
     fetch(chrome.runtime.getURL('html/card.html')).then(r => r.text()).then(html => {
+        html = html.replace("{{cardId}}","id1");
+        html = html.replace("{{recName}}","Collado");
         const container = document.createElement("div");
         container.setAttribute('class',"col-4");
         const div = escapeHTMLPolicy.createHTML(html);  
-        console.log(div);
         container.innerHTML = div;
         document.getElementById('citbCardRecContainer').appendChild(container);
     });
-
 }
 
-readElement();
+const waitingForRec = () => {
+    chrome.storage.sync.get('newRecord', (result) => {        
+        createRecordCard();
+    });
+}
+
+setInterval(waitingForRec,5000);
