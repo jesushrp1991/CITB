2e5423a7dd84e977dfd4176cc2c5ce57f08280da
noel <ndcg9105@gmail.com>
2022-01-03 15:33:47 +0100

    nueva api key

--- CITB_Rec/config/environment.js
+++ CITB_Rec/config/environment.js
@@ -4,7 +4,7 @@ const environment = {
     videoRecordMimeType : 'video/webm; codecs=vp9',
     audioRecordMimeType : 'audio/webm; codecs=vp9',
     upLoadToDrive : true,
-    API_KEY : 'AIzaSyDhLKHKTBWjlDSrRLPY_-kvgV0xcJH7qd0',
+    API_KEY : 'AIzaSyD-1Qv29qBstORU_Olt0ESCe5GLLKyM8as',
     DISCOVERY_DOCS : ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest","https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
     videoCaptureModes : ['screen','audio'],
     videoCaptureModesForMac : ['tab','audio'],
