0cb79c2efa823291c2ac00e79fe14efc8e5d5213
noel <ndcg9105@gmail.com>
2022-01-04 17:12:43 +0100

    error handling

--- CITB_Rec/js/errorHandling.js
+++ CITB_Rec/js/errorHandling.js
@@ -1,5 +1,5 @@
 import { environment } from "../config/environment.js";  
-
+import { reset } from '.recTimer.js';
 const errorHandling = (error) => {
     console.log(error);
     let inf = JSON.stringify(error,null,3);  
