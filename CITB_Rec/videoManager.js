7a69ffa289f385dfb689e532fcb0d7ea2e906b4c
noel <ndcg9105@gmail.com>
2022-01-12 21:41:48 +0100

    drag arreglado (funcionaba)

--- CITB_Rec/videoManager.js
+++ CITB_Rec/videoManager.js
@@ -187,7 +187,7 @@ const dragElement = (element) => {
   document.addEventListener("drop", (event) => {
       event.preventDefault();
       if ( event.target.className.includes("dropzone")) {
-        console.log(dragged);
+        console.log(dragged, dragged.id);
         let node = document.getElementById(dragged.id);
         node.parentNode.removeChild(node);
         event.target.style.opacity = "";
