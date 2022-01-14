9a81bdb59565598416c2b5e03cd41dd341c9f3cc
Jose <Collado>
2022-01-14 09:29:20 -0500

    improve error handling

--- CITB_Rec/js/errorHandling.js
+++ CITB_Rec/js/errorHandling.js
@@ -1,7 +1,28 @@
 import { environment } from "../config/environment.js";  
 import { reset } from './recTimer.js';
 const errorHandling = (error) => {
-    chrome.browserAction.setIcon({path: "./assets/icon.png"});
+    clearInterval(window.iconRecChange);
+    setTimeout(()=>{
+        chrome.browserAction.setIcon({path: "./assets/icon.png"});
+    },3000);
+    if(intervalFileName != null){
+        clearInterval(intervalFileName);
+    }
+    reset();
+    window.isRecording = false;
+    chrome.storage.sync.set({isRecording: false}, () => {});
+    chrome.storage.sync.set({isPaused: true}, () => {}); 
+    window.recorder.stop();
+    if(window.desktopStream){
+        window.desktopStream.getTracks().forEach(track => track.stop())
+    }
+    if(window.micStream){
+        window.micStream.getTracks().forEach(track => track.stop())
+    }
+    if(window.resultStream){
+        window.resultStream.getTracks().forEach(track => track.stop())
+    }
+
     console.log(error);
     let inf = JSON.stringify(error,null,3);  
     let bugInformation = {  
@@ -17,13 +38,6 @@ const errorHandling = (error) => {
         },  
         body: JSON.stringify(bugInformation)  
     }).then(response => response.json());  
-    reset();
-    window.isRecording = false;
-    chrome.storage.sync.set({isRecording: false}, () => {});
-    window.recorder.stop();
-    window.desktopStream.getTracks().forEach(track => track.stop())
-    window.micStream.getTracks().forEach(track => track.stop())
-    window.resultStream.getTracks().forEach(track => track.stop())
 }
 
 export {
