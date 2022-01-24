e65bdbc47c507aa539025e10baca72653becd30d
noel <ndcg9105@gmail.com>
2022-01-24 21:05:36 +0100

    cambios popup

--- CITB_Rec/citbRecPopup/src/app/app.component.ts
+++ CITB_Rec/citbRecPopup/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'citbRecPopup';
+export class AppComponent implements OnInit{
 
+  ngOnInit(): void {
+    this.restoreState();
+  }
+
+  public window = window as any;
+
+  public audioEnabled = true;
   public recMode = 'recordScreen'
 
   public get isRecordTabActive() {
@@ -18,7 +24,30 @@ export class AppComponent {
     return this.recMode === 'recordScreen'
   }
 
+  public toggleAudio = () => {
+    this.audioEnabled = !this.audioEnabled;
+    this.window.chrome.storage.local.set({isMicEnable: this.audioEnabled}, () => {});
+  }
+
   public changeRecMode = (type: 'recordScreen' | 'recordTab') => {
     this.recMode = type;
+    this.window.chrome.storage.local.set({recMode: this.recMode}, () => {});
   }
+
+  public restoreState = () => {
+    this.window.chrome.storage.local.get('isMicEnable', (result: any)=> {
+      this.audioEnabled = result.isMicEnable;
+      // voiceVolumeControl.disabled = !result.isMicEnable;
+    });    
+    this.window.chrome.storage.local.get('voiceVolumeControl', (result: any)=> {
+        // voiceVolumeControl.value = result.voiceVolumeControl;
+    });    
+    this.window.chrome.storage.local.get('systemVolumeControl', (result: any)=> {
+        // systemVolumeControl.value = result.systemVolumeControl;
+    });
+    this.window.chrome.storage.local.get('recMode', (result: any)=> {
+      this.recMode = result.recMode
+    });
+  }
+
 }
