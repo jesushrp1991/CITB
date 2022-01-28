0100208a434634cb6ff1060f8ff9ca722fa1faf5
Jose Fco. Collado <jfcollado.figueroa@gmail.com>
2022-01-28 07:59:26 -0500

    select kind of record working!!!

--- CITB_Rec/citbRecPopup/src/app/buttons/recordScreen/recordButton.component.ts
+++ CITB_Rec/citbRecPopup/src/app/buttons/recordScreen/recordButton.component.ts
@@ -11,13 +11,14 @@ export class RecordButtonComponent extends BaseButton {
     @Input() public recordKind: string = 'recordTab';
     public active = false;
     private _activeRecordMode: string = 'recordTab';
+
     @Input() public get activeRecordMode () {
         return this._activeRecordMode;
     } 
     public set activeRecordMode (value: string) {
         console.log("ACTIVE RECORD MODE", value)
         this._activeRecordMode = value; 
-        this.active = this.activeRecordMode == this.recordKind
+        this.active = ( this.activeRecordMode == this.recordKind );
         if (this.active) {
             this.changeRecMode();
         }
@@ -25,10 +26,6 @@ export class RecordButtonComponent extends BaseButton {
 
     private changeRecMode = () => {
         this.window.chrome.storage.local.set({ recMode: this.recordKind }, () => {});
-    }
-
-    public get recordButtonImg() {
-        return this.recordKind == 'recordTab' ? 'assets/recordTab.svg' : 'assets/recordScreen.svg'
     }
 
 }
