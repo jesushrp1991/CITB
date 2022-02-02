6f6e4539e3e4cac0b6dad73ba42f07fc3016b5fd
Jose Fco. Collado <jfcollado.figueroa@gmail.com>
2022-02-02 08:15:58 -0500

    WIP: Resumable Upload!!!

--- CITB_Rec/js/tsFiles/ResumableUpload2.ts
+++ CITB_Rec/js/tsFiles/ResumableUpload2.ts
@@ -1,5 +1,4 @@
 type ResumableDownloadOptions = {
-  // resource: [number];
   accessToken: string;
   fileName: string;
   mimeType: string;
@@ -13,7 +12,7 @@ class ResumableUpload2 {
   location: string = "";
   chunkSize: number = 256 * 1024;
   startBuffer: number = 0;
-  endBuffer: number = this.chunkSize; //Initial value;
+  endBuffer: number = this.chunkSize;
   fileTotalSize: number = 0;
   file: Blob;
   options: ResumableDownloadOptions;
@@ -31,7 +30,7 @@ class ResumableUpload2 {
   public initializeRequest(): Promise<Headers> {
     return new Promise((resolve, reject) => {
       let metadata = {
-        mimeType: this.options.mimeType,
+        // mimeType: this.options.mimeType,
         name: this.options.fileName,
         parents: this.options.parentFolderId,
       };
@@ -44,7 +43,6 @@ class ResumableUpload2 {
         },
       })
         .then((res) => {
-          //console.log("http initialize",res)
           if (res.status != 200) {
             res.json().then((e) => reject(e));
             return;
@@ -54,40 +52,35 @@ class ResumableUpload2 {
           resolve(res.headers);
         })
         .catch((err) => {
-          //console.log(err);
+          console.log(err);
           reject(err);
         });
     });
   }
 
   public nextChunk() {
-    //console.log("NEXT CHUNK",this.startBuffer, this.endBuffer);
     try {
       return this.file.slice(this.startBuffer, this.endBuffer);
     } catch (error) {
-      //console.log(error);
+      console.log(error);
     }
   }
 
   public async start() {
-    //console.log("START!!!",this.fileTotalSize)
     try {
       const len = Math.ceil(this.fileTotalSize / this.chunkSize);
       for (let index = 0; index < len; index++) {
-        //console.log("before next chunk")
         let nextChunk = this.nextChunk();
-        //console.log("start next chunk",a)
         await this.doUpload(nextChunk);
         nextChunk = null; //Asegurandonos de limpiar la memoria.
       }
     } catch (error) {
-      //console.log(error);
+      console.log(error);
     }
   }
 
   public doUpload = (element) => {
     return new Promise((resolve, reject) => {
-      //console.log("Init FETCH")
       const contentRange: string =
         "bytes " +
         String(this.startBuffer) +
@@ -106,26 +99,14 @@ class ResumableUpload2 {
         .then((res) => {
           const status = res.status;
           if (status == 308) {
-            console.log("fetch result 308");
             let range = res.headers.get("Range");
             let lastUploadedByte = range.split("-");
             this.startBuffer = parseInt(lastUploadedByte[1]) + 1;
-            this.endBuffer = this.startBuffer + this.chunkSize;
-
-            if(this.endBuffer > this.fileTotalSize){
-              console.log("ENtroo!!!")
-              // let diference = this.fileTotalSize - this.startBuffer;
-              // this.endBuffer = this.startBuffer + diference;
-              this.endBuffer = this.fileTotalSize + 1;
-            }
+            this.endBuffer = Math.min(this.startBuffer + this.chunkSize,this.fileTotalSize);
             resolve({ status: "Next", result: res });
           } 
           else if (status == 200) {
             console.log("fetch result 200");
-            // this.startBuffer = this.endBuffer + 1;
-            // this.endBuffer += 1024 * 256 * 2;
-            // console.log(this.startBuffer);
-            // res.json().then((r) => resolve({ status: "Done", result: r }));
             resolve({ status: "Done", result: res });
           } 
           else {
