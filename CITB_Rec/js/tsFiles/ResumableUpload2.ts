667fa4d2da946d9b91f9ff0c0bc33215103a5d26
Jose Fco. Collado <jfcollado.figueroa@gmail.com>
2022-02-01 23:53:13 -0500

    ResumableUpload working except last chunk!!!

--- CITB_Rec/js/tsFiles/ResumableUpload2.ts
+++ CITB_Rec/js/tsFiles/ResumableUpload2.ts
@@ -7,96 +7,132 @@ type ResumableDownloadOptions = {
 };
 
 class ResumableUpload2 {
-  endpoint: string = "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable";
-  start: number = 0;
-  cantRetries : number = 0;
+  endpoint: string =
+    "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable";
+  cantRetries: number = 0;
+  location: string = "";
+  chunkSize: number = 256 * 1024;
+  startBuffer: number = 0;
+  endBuffer: number = this.chunkSize; //Initial value;
+  fileTotalSize: number = 0;
+  file: Blob;
+  options: ResumableDownloadOptions;
 
-  constructor() {
-  }
-
-  public async upload(
-     file: Blob
-     ,totalSize: number
-    ,opciones: ResumableDownloadOptions
+  constructor(
+    file: Blob,
+    options: ResumableDownloadOptions,
+    fileTotalSize: number
   ) {
-    try {
-      callback("Status", "Iniciando");
-      const head = await this.initializeRequest(opciones);
-      const location = head.get("location");
-      callback("getLocation", location ?? "");
-      let range: string = "bytes " + this.start + "-" + this.start + 256 + "/" + this.totalSize;
-      let fileAsArray = await this.getArrayBuffer(file);
-      let httpResponse: Response = await this.doUpload(fileAsArray, range, location ?? "");
-
-      if(httpResponse.status == 200){
-        this.start += 256 * 1024; //if succesfully upload, asumiendo que los chunks son de 256 kb todo el tiempo
-        console.log("http 200")
-        // callback("Done",null);
-        return "new pedazo"
-      }
-      if(httpResponse.status == 308){
-        console.log("http 308")
-        if(this.cantRetries >= 5){
-            this.cantRetries = 0;
-            throw "Error, tratar luego"
-        }
-        else{
-            this.start = this.start + 256 - (parseInt(httpResponse.headers.get('Range') ?? "0"));
-            this.upload(file,callback,opciones);
-            this.cantRetries ++;
-        }
-      }
-    } catch (error) {
-      callback("Ha ocurrido el stge error:", JSON.stringify(error));
-    }
+    this.file = file;
+    this.options = options;
+    this.fileTotalSize = fileTotalSize;
   }
 
-  public initializeRequest(
-    options: ResumableDownloadOptions
-  ): Promise<Headers> {
+  public initializeRequest(): Promise<Headers> {
     return new Promise((resolve, reject) => {
       let metadata = {
-        mimeType: options.mimeType,
-        name: options.fileName,
-        parents: options.parentFolderId,
+        mimeType: this.options.mimeType,
+        name: this.options.fileName,
+        parents: this.options.parentFolderId,
       };
       fetch(this.endpoint, {
         method: "POST",
         body: JSON.stringify(metadata),
         headers: {
-          Authorization: "Bearer " + options.accessToken,
+          Authorization: "Bearer " + this.options.accessToken,
           "Content-Type": "application/json",
         },
       })
         .then((res) => {
-        console.log("http initialize",res)
+          //console.log("http initialize",res)
           if (res.status != 200) {
             res.json().then((e) => reject(e));
             return;
           }
+          this.location = res.headers.get("location");
+          console.log(this.location, res.headers);
           resolve(res.headers);
         })
         .catch((err) => {
+          //console.log(err);
           reject(err);
         });
     });
   }
 
-  private doUpload(data:ArrayBuffer,range:string, url:string): Promise<Response> {
-    return new Promise(function (resolve, reject) {
-      fetch(url, {
+  public nextChunk() {
+    //console.log("NEXT CHUNK",this.startBuffer, this.endBuffer);
+    try {
+      return this.file.slice(this.startBuffer, this.endBuffer);
+    } catch (error) {
+      //console.log(error);
+    }
+  }
+
+  public async start() {
+    //console.log("START!!!",this.fileTotalSize)
+    try {
+      const len = Math.ceil(this.fileTotalSize / this.chunkSize);
+      for (let index = 0; index < len; index++) {
+        //console.log("before next chunk")
+        let nextChunk = this.nextChunk();
+        //console.log("start next chunk",a)
+        await this.doUpload(nextChunk);
+        nextChunk = null; //Asegurandonos de limpiar la memoria.
+      }
+    } catch (error) {
+      //console.log(error);
+    }
+  }
+
+  public doUpload = (element) => {
+    return new Promise((resolve, reject) => {
+      //console.log("Init FETCH")
+      const contentRange: string =
+        "bytes " +
+        String(this.startBuffer) +
+        "-" +
+        String((this.endBuffer - 1)) +
+        "/" +
+        this.fileTotalSize;
+      console.log("contentRange",contentRange);
+      fetch(this.location, {
         method: "PUT",
-        body: data,
-        headers: { "Content-Range": range },
+        body: element,
+        headers: {
+          "Content-Range": contentRange,
+        },
       })
         .then((res) => {
           const status = res.status;
           if (status == 308) {
-            resolve(res);
-          } else if (status == 200) {
-            res.json().then((res) => resolve(res));
-          } else {
+            console.log("fetch result 308");
+            let range = res.headers.get("Range");
+            let lastUploadedByte = range.split("-");
+            this.startBuffer = parseInt(lastUploadedByte[1]) + 1;
+            this.endBuffer = this.startBuffer + this.chunkSize;
+
+            if(this.endBuffer > this.fileTotalSize){
+              console.log("ENtroo!!!")
+              // let diference = this.fileTotalSize - this.startBuffer;
+              // this.endBuffer = this.startBuffer + diference;
+              this.endBuffer = this.fileTotalSize + 1;
+            }
+            resolve({ status: "Next", result: res });
+          } 
+          else if (status == 200) {
+            console.log("fetch result 200");
+            // this.startBuffer = this.endBuffer + 1;
+            // this.endBuffer += 1024 * 256 * 2;
+            // console.log(this.startBuffer);
+            // res.json().then((r) => resolve({ status: "Done", result: r }));
+            resolve({ status: "Done", result: res });
+          } 
+          else {
+            console.log("fetch result ??", res.status);
             res.json().then((err) => {
+              err.additionalInformation =
+                "When the file size is large, there is the case that the file cannot be converted to Google Docs. Please be care§l this.";
               reject(err);
               return;
             });
@@ -104,22 +140,10 @@ class ResumableUpload2 {
           }
         })
         .catch((err) => {
+          console.log("error fetch", err);
           reject(err);
           return;
         });
     });
-  }
-
-//   private getArrayBuffer (file: Blob) : ArrayBuffer | string {
-  private getArrayBuffer (file: Blob) : Promise<ArrayBuffer> {
-    return new Promise((resolve, reject) => {
-      var fileReader = new FileReader();
-      fileReader.onload = (event) =>{
-        const arrayBuffer = event.target!.result as ArrayBuffer;
-        resolve( arrayBuffer );
-      };
-      fileReader.readAsArrayBuffer(file);
-    })
-   
-  }
+  };
 } //END Class
