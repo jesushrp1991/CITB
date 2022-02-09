c43569cb88f047b4ed9fb4ec811310845715ed3e
Jose Fco. Collado <jfcollado.figueroa@gmail.com>
2022-02-09 17:13:01 -0500

    Add tag and pin methods

--- CITB_Rec/js/services.js
+++ CITB_Rec/js/services.js
@@ -28,7 +28,7 @@ const createVideo = async (dbToken, name, recordedDate) => {
   return response.json();
 };
 
-const updateVideo = async (dbToken, duration,videoLink, id) => {
+const updateVideo = async (dbToken, duration, videoLink, id) => {
   const response = await fetch(environment.backendURL + "video/" + id, {
     method: "PUT",
     headers: {
@@ -37,11 +37,61 @@ const updateVideo = async (dbToken, duration,videoLink, id) => {
     },
     body: JSON.stringify({
       duration: duration,
-      videoLink: videoLink
+      videoLink: videoLink,
     }),
   });
   return response.json();
 };
 
+const addTag = async (dbToken, idVideo, startTime) => {
+  const response = await fetch(
+    environment.backendURL + "video/" + idVideo + "/tag/start",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + dbToken,
+      },
+      body: JSON.stringify({
+        startTime: startTime,
+      }),
+    }
+  );
+  return response.json();
+};
 
-export { getDBToken, createVideo, updateVideo };
+const tagEndTime = async (dbToken, idVideo, idTag, endTime) => {
+  const response = await fetch(
+    environment.backendURL + "video/" + idVideo + "/tag/" + idTag + "/end",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + dbToken,
+      },
+      body: JSON.stringify({
+        endTime: endTime,
+      }),
+    }
+  );
+  return response.json();
+};
+
+const addMark = async (dbToken, idVideo, time) => {
+  const response = await fetch(
+    environment.backendURL + "video/" + idVideo + "/mark",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + dbToken,
+      },
+      body: JSON.stringify({
+        time: time,
+      }),
+    }
+  );
+  return response.json();
+};
+
+export { getDBToken, createVideo, updateVideo, addTag, tagEndTime, addMark };
