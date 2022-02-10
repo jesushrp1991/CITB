1d675f7f8a9e994a638adee0afc7f27a7aefadc7
Jose Fco. Collado <jfcollado.figueroa@gmail.com>
2022-02-10 15:13:29 -0500

    Reorganizando la carpeta

--- popup.js
+++ popup.js
@@ -1,152 +1,277 @@
-// Initialize select with default mode
-let buttonOn = document.getElementById("button1");
-let buttonChooseVideo = document.getElementById("button3");
-let button4WEB = document.getElementById("button4");
-let buttonChooseMic = document.getElementById("button5");
-let showActivated = false,
-  classActivated = false,
-  citbActivated,
-  webContainerActivated = true,
-  canChangeCameras,
-  globalState = false;
+import { checkTimer } from './js/timerBar.js';
 
-const getExtensionState = () => {
-  let isOpen = document.getElementById("buttonOnOff").innerText.toString();
-  return isOpen;
-};
+let isMicEnable = true;
 
-var onOfChekerCounter = 0;
-const onOfChecker = (tab) => {
-  const url = tab.url;
-  if (
-    url.includes("meet.google.com") ||
-    url.includes("teams.microsoft.com") ||
-    url.includes("teams.live.com") ||
-    url.includes("zoom.us") ||
-    url.includes("meet.jit.si")
-  ) {
-    chrome.tabs.executeScript(tab.id,{
-      code: 'isOpen = document.getElementById("buttonOnOff").innerText.toString();isOpen;'      
-    },(injectionResults) => {
-      injectionResults[0] == "true" ?
-                  globalState = true
-                  : globalState = false;
-      if(globalState){ 
-          buttonOn.setAttribute('class','buttonOnOff'); 
-      }else{ 
-        buttonOn.setAttribute('class','buttonOnOffDeactivate'); 
-      } 
-    });
-  }
-  //try again each second during 5 seconds
-  if (onOfChekerCounter < 5) {
-    onOfChekerCounter += 1;
-    setTimeout(() => {
-      onOfChecker(tab);
-    }, 1000);
-  } else {
-    onOfChekerCounter = 0;
-  }
-};
-const getOnOffState = async () => {
-  chrome.tabs.getSelected(null, (tab) => {
-    onOfChecker(tab);
-  });
-};
-getOnOffState();
-
-
-
-buttonOn.addEventListener("click", async () => {
-  chrome.tabs.getSelected(null, (tab) => {
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-      let url = tab.url;
-      if (
-        url.includes("meet.google.com") ||
-        url.includes("teams.microsoft.com") ||
-        url.includes("teams.live.com") ||
-        url.includes("zoom.us") ||
-        url.includes("meet.jit.si")
-      ) {
-        chrome.tabs.executeScript(tab.id,{
-          code: 'document.getElementById("buttonOnOff").click();'
+window.recMode = 'recordScreen';
+const sendMessage = (msg) =>{
+    chrome.runtime.sendMessage(msg);
+}
+const rec = (isTabForMac) =>{
+    let idMic;
+    isMicEnable || isMicEnable == undefined ? idMic = select.value : idMic = null
+    const request = { recordingStatus: 'rec' , idMic: idMic ,idTab : isTabForMac, recMode: window.recMode};
+    sendMessage(request);
+}
+const sendRecordCommand = () =>{
+    let userAgentData = navigator.userAgentData.platform.toLowerCase().includes('mac');
+    if(userAgentData){
+        chrome.tabs.getSelected(null, (tab) => {
+            rec(tab.id);
         });
-      }
-      setTimeout(() => {
-        getOnOffState();
-      }, 100);
-    });
-  });
-});
-
-const showContainer = () => {
-  let isOpen = document.getElementById("buttonOnOff").innerText.toString();
-  if (isOpen == "true") {
-    document.getElementById("buttonsContainer").style.visibility = "visible";
-    document.getElementById("pWebContainerState").innerText = "OPEN";
-  }
-};
-
-const setButtonWebContainerBackground = (isOpen) => {
-  console.log("setButtonWebContainerBackground",isOpen);
-  if (isOpen) {
-    button4WEB.className = "buttonOpen4WebContainer";
-  } else {
-    button4WEB.className = "button4WebContainer";
-  }
-};
-
-button4WEB.addEventListener("click", async () => {
-  chrome.tabs.getSelected(null, (tab) => {
-    console.log("click!!!",webContainerActivated);
-    if (webContainerActivated) {
-      chrome.tabs.executeScript(tab.id,{
-        code: 'document.getElementById("buttonsContainer").style.visibility = "hidden";document.getElementById("pWebContainerState").innerText = "CLOSE";'
-      });
-    } else {
-      chrome.tabs.executeScript(tab.id,{
-        code:"if(document.getElementById('buttonOnOff').innerText.toString() == 'true') { document.getElementById('buttonsContainer').style.visibility = 'visible'; document.getElementById('pWebContainerState').innerText = 'OPEN' }"
-      });
+    }else{
+        rec()
     }
-    webContainerActivated = !webContainerActivated;
-    setButtonWebContainerBackground(webContainerActivated);
-    console.log("click222!!!",webContainerActivated);
-  });
- 
-});
+}
 
-const chekWebContainerState = async () => {
-  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.tabs.getSelected(null, (tab) => {
-    chrome.tabs.executeScript(tab.id,{
-      code: 'isOpen = document.getElementById("pWebContainerState").innerText.toString(); isOpen;'
-    },(injectionResults)=>{
-        console.log(injectionResults)
-        injectionResults[0] == "OPEN"
-          ? (webContainerActivated = true)
-          : (webContainerActivated = false);
-          setButtonWebContainerBackground(webContainerActivated);
-      }
-    );
-  });
-};
-chekWebContainerState();
-
-buttonChooseMic.addEventListener("click", async () => {
-  chrome.tabs.getSelected(null, (tab) => {
-    chrome.tabs.executeScript(tab.id,{
-      code: 'document.getElementById("buttonPopup").click();'
+const getCurrentState = () =>{
+    chrome.storage.sync.get('isRecording', (result) => {
+        if (result.isRecording ){
+            displayRecordingMode();
+        }
+        // else{
+        //     displayNotRecordingMode();
+        // }
     });
-  });
+    chrome.storage.sync.get('isPaused', (result) => {        
+        if(result.isPaused ){
+            buttonPlayPause.setAttribute('class','play-icon icons');
+        }else{
+            buttonPlayPause.setAttribute('class','pause-icon icons');
+        }        
+    });
+    chrome.storage.sync.get('voice', (result) => {
+        if(result.voice){
+            buttonVoiceControl.setAttribute('class','voiceControl icons')
+        }else{
+            buttonVoiceControl.setAttribute('class','voiceControlOff icons');
+        }
+    })
+}
+
+let citbButtonsContainer = document.getElementById('citbButtonsContainer');
+let isCITBPanelVisible;
+const initialCITBPanelStatus = () =>{
+    chrome.storage.sync.get('isCITBPanelVisible', (result) => {
+        isCITBPanelVisible = result.isCITBPanelVisible;
+        if(result.isCITBPanelVisible){
+            citbButtonsContainer.classList.add('expanded');
+        }
+    });
+}
+
+
+const playPause = () =>{
+    const request = { recordingStatus: 'pause' };
+    if(buttonPlayPause.getAttribute('class').includes('pause-icon')){
+        buttonPlayPause.setAttribute('class','play-icon icons')
+    }else{
+        buttonPlayPause.setAttribute('class','pause-icon icons');
+    }
+    sendMessage(request);
+}
+
+const activateVoiceControl = () =>{
+    const request = { recordingStatus: 'voiceOpen' };
+    if(buttonVoiceControl.getAttribute('class').includes('voiceControlOff')){
+        buttonVoiceControl.setAttribute('class','voiceControl icons')
+        sendMessage(request);
+        chrome.runtime.openOptionsPage(()=>{});
+    }else{
+        const request = { recordingStatus: 'voiceClose' };
+        sendMessage(request);
+        buttonVoiceControl.setAttribute('class','voiceControlOff icons');
+    }
+}
+
+let select = document.getElementById('miclist');
+const populateMicSelect = async () => {
+    let micList;
+    try {
+        await navigator.mediaDevices.getUserMedia({audio: true})
+        micList = await navigator.mediaDevices.enumerateDevices();
+    } catch (error) {
+        activateVoiceControl();
+    }
+    micList = await navigator.mediaDevices.enumerateDevices();
+    let usableMic = micList.filter((x) =>  x.kind === "audioinput" && !x.label.includes('CITB'));    
+    let citb = micList.filter((x) => x.kind === "audioinput" && x.label.includes('CITB'));
+    let organizedMicList = [];
+    if(citb.length > 0){
+        organizedMicList.push(citb[0]);
+        organizedMicList = organizedMicList.concat(usableMic);
+    }else{
+        organizedMicList = usableMic;
+        document.getElementById('citbMissingAlert').classList.add('expanded');
+    }
+    while (select.options.length > 0) {                
+        select.remove(0);
+    }  
+    organizedMicList.forEach(element => {
+        var option = document.createElement("option");
+        option.text = element.label;
+        option.value = element.deviceId;
+        select.add(option);
+    });
+}
+
+const checkAut = () => {
+    const request = { recordingStatus: 'checkAuth' };
+    sendMessage(request);
+}
+
+const showRecList = () => {
+    const request = { recordingStatus: 'showRecList' };
+    sendMessage(request);
+}
+
+let buttonRec = document.getElementById('button-rec');
+let newTimerPanel = document.getElementById('panelTimer');
+
+const displayRecordingMode = () =>{
+    if(document.getElementById('citbMissingAlert').style.display == 'block'){
+        buttonRec.classList.remove('panel-timer-container');
+        buttonRec.classList.add('panel-timer-container-alert');
+    }
+    buttonRec.classList.remove('button-active');
+    buttonRec.classList.add('button-hide');
+    newTimerPanel.classList.remove('rec-timer-hide');
+    newTimerPanel.classList.add('rec-timer-active');
+
+    audioPanel.style.display = 'none';
+    volumeControl.style.display = 'block';
+    
+    chrome.storage.local.get('isMicEnable', (result)=> {
+        voiceVolumeControl.disabled = !result.isMicEnable;
+    });    
+    chrome.storage.local.get('voiceVolumeControl', (result)=> {
+        voiceVolumeControl.value = result.voiceVolumeControl;
+    });    
+    chrome.storage.local.get('systemVolumeControl', (result)=> {
+        systemVolumeControl.value = result.systemVolumeControl;
+    });
+    chrome.storage.local.get('recMode', (result)=> {
+        if(result.recMode == 'recordTabs')
+            recordTabs.click();
+        else
+            recordScreen.click();
+    });
+}
+buttonRec.addEventListener('click',() =>{
+    displayRecordingMode();
+    sendRecordCommand();
+})
+
+const displayNotRecordingMode = () =>{
+    buttonRec.classList.remove('button-hide');
+    buttonRec.classList.add('button-active');
+    newTimerPanel.classList.remove('rec-timer-active');
+    newTimerPanel.classList.add('rec-timer-hide');
+
+    // audioPanel.style.display = 'none';
+    audioPanel.style.display = 'block';
+    volumeControl.style.display = 'none';
+    chrome.storage.local.set({voiceVolumeControl: 1});
+    chrome.storage.local.set({systemVolumeControl: 1});
+    voiceVolumeControl.disabled = false;
+    chrome.storage.local.set({isMicEnable: true}, () => {});
+    chrome.storage.local.set({recMode: 'recordTabs'}, () => {});
+}
+let buttonStop = document.getElementById("stopButton");
+buttonStop.addEventListener('click',()=>{
+    displayNotRecordingMode();
+    sendRecordCommand();
 });
 
-const choose_Video = () => {
-  document.getElementById("buttonPopupVideo").click();
-};
-buttonChooseVideo.addEventListener("click", async () => {
-  chrome.tabs.getSelected(null, (tab) => {
-    chrome.tabs.executeScript(tab.id,{
-        code: 'document.getElementById("buttonPopupVideo").click();'
-    });
-  });
+let buttonPlayPause = document.getElementById("playPauseButton");
+buttonPlayPause.disabled = true;
+buttonPlayPause.addEventListener('click',playPause);
+
+let buttonVoiceControl = document.getElementById("voiceControlButton");
+buttonVoiceControl.addEventListener('click',activateVoiceControl);
+
+let buttonRecList = document.getElementById("recListButton");
+buttonRecList.addEventListener('click',showRecList);
+
+let recordTabs = document.getElementById("recordTabs");
+let recordScreen = document.getElementById("recordScreen");
+
+
+recordTabs.addEventListener('click',()=>{
+    recordTabs.classList.add('selected-icon');
+    recordScreen.classList.remove('selected-icon');
+    window.recMode = 'recordTabs'
+    chrome.storage.local.set({recMode: window.recMode}, () => {});
+
 });
+
+recordScreen.addEventListener('click',()=>{
+    recordScreen.classList.add('selected-icon');
+    recordTabs.classList.remove('selected-icon');
+    window.recMode = 'recordScreen'
+    chrome.storage.local.set({recMode: window.recMode}, () => {});
+});
+
+const checkCITBPanelStatus = () =>{
+    if(isCITBPanelVisible){
+        citbButtonsContainer.classList.remove('expanded');
+        // citbButtonsContainer.setAttribute('class','')
+        citbButtonsContainer.style.border = 'none';
+    }
+    else{
+        citbButtonsContainer.classList.add('expanded');
+        // citbButtonsContainer.setAttribute('class','expanded');
+        citbButtonsContainer.style.border = '';
+
+    }
+    isCITBPanelVisible = !isCITBPanelVisible;
+    chrome.storage.local.set({isCITBPanelVisible: isCITBPanelVisible}, () => {});
+}
+
+let citbOptions = document.getElementById('citbOptions');
+citbOptions.addEventListener('click',checkCITBPanelStatus)
+
+const micOn = () =>{
+    checkboxMic.classList.add('mic-on');
+    checkboxMic.classList.remove('mic-off');
+    voiceVolumeControl.disabled = false;
+    chrome.storage.local.set({isMicEnable: true}, () => {});
+}
+
+const micOff = () =>{
+    checkboxMic.classList.remove('mic-on');
+    checkboxMic.classList.add('mic-off');
+    voiceVolumeControl.disabled = true;
+    chrome.storage.local.set({isMicEnable: false}, () => {});
+}
+
+let checkboxMic = document.getElementById('checkboxMic');
+checkboxMic.addEventListener('click',()=>{
+    isMicEnable ? micOff() : micOn();
+    isMicEnable = !isMicEnable;
+})
+
+let volumeControl = document.getElementById('volumeControl');
+let audioPanel = document.getElementById('audioPanel');
+
+let voiceVolumeControl = document.getElementById('voiceVolumeControl');
+voiceVolumeControl.addEventListener('change',()=>{
+    const request = { recordingStatus: 'changeVoiceVolume' , volume: voiceVolumeControl.value};
+    sendMessage(request);
+    chrome.storage.local.set({voiceVolumeControl: voiceVolumeControl.value});
+})
+
+let systemVolumeControl = document.getElementById('systemVolumeControl');
+systemVolumeControl.addEventListener('change',()=>{
+    console.log(systemVolumeControl.value);
+    const request = { recordingStatus: 'changeSystemVolume' , volume: systemVolumeControl.value};
+    sendMessage(request);
+    chrome.storage.local.set({systemVolumeControl: systemVolumeControl.value});
+})
+
+checkAut();
+populateMicSelect();
+checkTimer();
+getCurrentState();
+initialCITBPanelStatus();
+setInterval(getCurrentState,2000);
+
